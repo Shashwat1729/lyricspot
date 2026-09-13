69e5dad59dfa9f175be88af5a837ba998d7121bc
Shashwat Bajpai <shashwatbajpai1729@gmail.com>
2026-09-13 13:53:38 +0530

    chore: remove QA harnesses

--- qa_wait_next.js
+++
@@ -1,5 +0,0 @@
-﻿setTimeout(() => {
-  const { execSync } = require("child_process");
-  execSync("node D:\\personal\\projects\\Music_cont\\qa_final.js", { stdio: "inherit" });
-}, 90000);
-console.log("waiting 90s for Pages d2620d6...");
