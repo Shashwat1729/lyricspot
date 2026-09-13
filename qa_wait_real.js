7bc8a3ceea2690c5e3e31e7984b7c82f5318b44b
Shashwat Bajpai <shashwatbajpai1729@gmail.com>
2026-09-13 13:20:46 +0530

    chore: remove QA harnesses

--- qa_wait_real.js
+++
@@ -1,4 +0,0 @@
-﻿setTimeout(() => {
-  require("child_process").execSync("node D:\\personal\\projects\\Music_cont\\qa_loop1.js", { stdio: "inherit" });
-}, 90000);
-console.log("waiting 90s for Pages deploy...");
